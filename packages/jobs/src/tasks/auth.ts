15b9c0da8e1938a84da9fc99bc26907114725b86
phoenixdahdev <ayomikuntemitope.araoye@gmail.com>
2026-02-11 21:17:20 -0500

    feat: authentication and emails

---
+++ packages/jobs/src/tasks/auth.ts
@@ -0,0 +1,32 @@
+import { nanoid } from 'nanoid'
+import { resend } from '@jobs/utils/resend'
+import { render } from '@react-email/render'
+import { schemaTask } from '@trigger.dev/sdk'
+import { emailVerificationSchema, } from '@jobs/schema'
+import { VerificationOTPEmail } from '@hackhyre/email/emails/verification-otp'
+
+export const sendVerificationEmail = schemaTask({
+    id: 'send-verification-email',
+    schema: emailVerificationSchema,
+    maxDuration: 30,
+    queue: {
+        concurrencyLimit: 10,
+    },
+    run: async ({ otp, email }) => {
+        const html = await render(VerificationOTPEmail({ otp, type: 'email-verification' }))
+        const { data, error } = await resend.emails.send({
+            headers: {
+                'X-Entity-Ref-ID': nanoid(),
+            },
+            from: 'Support <support@fynix.dev>',
+            to: [email],
+            subject: 'Your Verification Code',
+            html,
+        })
+
+        if (error) {
+            throw error
+        }
+        return { success: true, data }
+    },
+})
